2bfb528f54d706212822f2f74578842dfb609628
Yash-Gotherwal <144876278+Yash-Gotherwal@users.noreply.github.com>
2024-03-07 16:30:13 -0600

    added digital write for single prototype leg

--- ENEL 417 Docs/HM10_base_sketch.ino
+++ ENEL 417 Docs/HM10_base_sketch.ino
@@ -1,9 +1,17 @@
 #define CUSTOM_SETTINGS
 #define INCLUDE_GAMEPAD_MODULE
+#include <Servo.h>
 #include <Dabble.h>
+
+Servo coxa;
+Servo femur;
+Servo tibia;
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(250000);      // make sure your Serial Monitor is also set at this baud rate.
+ // put your setup code here, to run once:
+  coxa.attach(7);
+  femur.attach(8);
+  tibia.attach(9);
+  Serial.begin(9600);      // make sure your Serial Monitor is also set at this baud rate.
   Dabble.begin(9600);      //Enter baudrate of your bluetooth.Connect bluetooth on Bluetooth port present on evive.
 }
 
@@ -13,20 +21,32 @@ void loop() {
   if (GamePad.isUpPressed())
   {
     Serial.println("UP");
+    coxa.write(90);
+    femur.write(90);
+    tibia.write(90);
   }
 
   else if (GamePad.isDownPressed())
   {
     Serial.println("DOWN");
+    coxa.write(100);
+    femur.write(100);
+    tibia.write(100);
   }
 
   else if (GamePad.isLeftPressed())
   {
     Serial.println("Left");
+    coxa.write(110);
+    femur.write(110);
+    tibia.write(110);
   }
 
   else if (GamePad.isRightPressed())
   {
     Serial.println("Right");
+    coxa.write(120);
+    femur.write(120);
+    tibia.write(120);
   }
 }
